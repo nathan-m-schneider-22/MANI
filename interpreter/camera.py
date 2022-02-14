54d8343e7db8c4f7702e3fbf2b71cdbff6936677
nathan-m-schneider-22 <nathan.m.schneider.22@dartmouth.edu>
2022-02-14 06:03:28 -0500

    moving files + imports

--- interpreter/camera.py
+++ interpreter/camera.py
@@ -1,5 +1,4 @@
 import cv2
-
 cap = cv2.VideoCapture(0)
 
 # Camera class for all camera-related code
--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,6 +1,8 @@
 from .camera import Camera
 import cv2
 import time
+from model.LetterPredictor import LetterPredictor
+
 # Interpreter class to parse images into signs, and build signs
 
 
@@ -8,15 +10,20 @@ class Interpreter:
     def __init__(self, display_instance):
         self.display_instance = display_instance
         self.camera = Camera()
+        checkpoint_path = "model/sample-asl-combodata.ckpt"
+        self.model = LetterPredictor(checkpoint_path)
 
     # Parses the current frame from ASL to a letter
     def parse_frame(self):
         frame = self.camera.capture_image()
-        # cv2.imshow('frame', frame) # display the image if you want
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     exit(1)
-        # ML STUFF GOES HERE
-        result = "e"
+        cv2.imshow('frame', frame)
+        k = cv2.waitKey(1)
+        if k % 256 == 27:
+            print("Escape hit, closing...")
+            exit(0)
+
+        result = self.model.predict(frame)  # pass to Letter Predictor model
+        print(result)
         return result
 
     # Wait for a user to initiate an input, returns when the user is about to give an input, runs on FSM
