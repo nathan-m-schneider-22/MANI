9e8570f27557f24921f41d106a435008bccf7f5f
Sam Tabrisky <samj.tabs@gmail.com>
2022-02-21 09:17:28 -0500

    new shortcuts

--- virtual_assistant/shortcuts.py
+++ virtual_assistant/shortcuts.py
@@ -1,7 +1,12 @@
 import time
 
-shortcuts = {"BBC": "What is the news from the BBC?"}
-answers = {"BBC": "Unable to connect to the BBC"}
+shortcuts = {"BBC": "What is the news from the BBC?",
+            "WH": "What is the weather in Hanover, New Hampshire?",
+            "POKI": "How many pounds in a kilogram?",
+            "HOL": "When is the next federal holiday?",
+            "CD": "What is the current date?",
+            "DAN": "What is the definition of anthropomorphize?",
+            "HLW": "How large is a blue whale?"}
 
 
 def check_shortcut(input):
@@ -12,5 +17,5 @@ def check_shortcut(input):
         return None
 
 
-def get_shortcut_answer(input):
-    return answers[input]
+# def get_shortcut_answer(input):
+#     return answers[input]
