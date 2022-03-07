deb4a3196fa4846afa774aef0a1440436694d011
Sam Tabrisky <samj.tabs@gmail.com>
2022-03-06 23:05:09 -0500

    added smart home shortcuts

--- virtual_assistant/shortcuts.py
+++ virtual_assistant/shortcuts.py
@@ -7,7 +7,14 @@ shortcuts = {"bc": "What is the news from the BBC?",
              "cd": "What is the current date?",
              "dan": "What is the definition of anthropomorphize?",
              "hlw": "How large is a blue whale?",
-             "cal": "What'on on my calendar"}
+             "cal": "What'on on my calendar",
+             "cnn": "Whats the news from CNN?",
+             "gs": "What is the nearest grocery store?",
+             "gl": "Turn on the garage lights",
+             "al": "Arm the house alarm",
+             "am": "Add milk to my shopping list",
+             "tse": "Set the thermostat to 68 degrees",
+             "asam": "Set an alarm for 7 AM"}
 
 
 def check_shortcut(input):
