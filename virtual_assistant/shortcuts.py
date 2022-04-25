655405691284ca6d0053892102eaee9f172e4637
et22 <43649954+et22@users.noreply.github.com>
2022-04-25 16:15:56 -0400

    Read shortcuts from csv

--- virtual_assistant/shortcuts.py
+++ virtual_assistant/shortcuts.py
@@ -1,19 +1,10 @@
-shortcuts = {"bc": "What is the news from the BBC?",
-             "wh": "What is the weather in Hanover, New Hampshire?",
-             "poki": "How many pounds in a kilogram?",
-             "hol": "When is the next federal holiday?",
-             "cd": "What is the current date?",
-             "dan": "What is the definition of anthropomorphize?",
-             "hlw": "How large is a blue whale?",
-             "cal": "What'on on my calendar",
-             "cnn": "Whats the news from CNN?",
-             "gs": "What is the nearest grocery store?",
-             "gl": "Turn on the garage lights",
-             "al": "Arm the house alarm",
-             "am": "Add milk to my shopping list",
-             "tse": "Set the thermostat to 68 degrees",
-             "asam": "Set an alarm for 7 AM"}
+import csv
 
+shortcuts = dict()
+with open('shortcuts.csv', newline='\n') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        shortcuts[row["Shortcut"]] = row["Query"]
 
 def check_shortcut(input):
     input = input.lower()
